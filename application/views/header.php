485d15e55a252c8055efce0dbb6e0fa703388ea7
Lebon Charles <s208059956@nmmu.ac.za>
2017-09-17 23:41:12 +0200

    home

---
+++ application/views/header.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Ruth</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="<?php base_url('assets/css/style.css',null);?>" >
+</head>
+<body>
+<!--Open Class container for the whole page-->
+<div class="container">
